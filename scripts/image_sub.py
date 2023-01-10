6fc0db5d116d2ad25142896b64c7fed7a193adcf
nata-vito <nata14vitorino@hotmail.com>
2023-01-10 11:23:49 -0300

    Perform image capture from a topic, but not yet supported by Mediapipe

--- scripts/image_sub.py
+++ scripts/image_sub.py
@@ -1,34 +1,42 @@
 #!/usr/bin/env python2
+import os
 import sys
 import rospy
 import cv2 as cv
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-#import hand_tracking as ht
+import tes as test
 
 class image_converter:
     def __init__(self):
+        # To conver image-msg to Cv Type
         self.bridge = CvBridge()
+        # Topic to read the image msg data
         self.image_sub = rospy.Subscriber("camera", Image, self.image_callback)
         self.img = None
 
     def image_callback(self, msg):
         try:
-            self.img = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-            print(self.img)
-            self.showImage()
+            self.img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+            #print(self.img)
         except CvBridgeError as e:
             rospy.logerr("CvBridge Error: {0}".format(e))
-            
-        
+                
+        self.showImage(0) 
     
-    def showImage(self):
+    
+    def showImage(self, hand_side):
         if self.img is None:
             print("Could not read the image.")
-        else:       
-            cv.imshow("testing image", self.img)
-            cv.waitKey(0)
-        
+        else:  
+            """ path = r'/home/rota2030/Documents/ws_see_how/src/see_how_pkg/src/scripts/image'
+            os.chdir(path)
+            cv.imwrite('imgTest.jpg', self.img)
+            cv.imshow("Image Window", self.img)      
+            cv.waitKey(3)"""
+            
+            test.leftHand(self.img)
+            return self.img
        
         
         
@@ -41,7 +49,7 @@ def main(args):
     except KeyboardInterrupt:
         print("Shutting down")
     
-    cv.destroyAllWindows()
+    #cv.destroyAllWindows()
     
 if __name__ == "__main__":
     main(sys.argv)
